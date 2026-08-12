407561d201ab0216daaa03d55ebb9927fda860b6
adn-dodo <elkawasadn@gmail.com>
2026-08-12 23:32:42 +0300

    file_io.h

--- file_io.h
+++ file_io.h
@@ -3,7 +3,16 @@
 
 #include <stdint.h>
 
+/*
+ * Reset all temporary UserFS file descriptors.
+ * Member 1 should call this during mount and unmount.
+ */
 void descriptor_table_reset(void);
+
+/*
+ * Return 1 if inode_number is currently open, otherwise return 0.
+ * Member 3 uses this to prevent deletion of an open file.
+ */
 int inode_is_open(uint32_t inode_number);
 
 #endif
